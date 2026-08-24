ef226163fa5bbca32ad761ab64039651056ff4fd
timceo2006-source <timceo2006@gmail.com>
2026-08-24 13:13:43 +0700

    Update acceptTrade.lua

--- acceptTrade.lua
+++ acceptTrade.lua
@@ -10,34 +10,33 @@ local playerGui = player:WaitForChild("PlayerGui")
 task.spawn(function()
     while true do
         pcall(function()
-            for _, gui in ipairs(playerGui:GetChildren()) do
-                if gui:IsA("ScreenGui") then
-                    local yesButton = gui:FindFirstChild("Yes", true)
+            local tabs = playerGui:FindFirstChild("Tabs")
+            if tabs then
+                local areYouSure = tabs:FindFirstChild("Are You Sure")
+                if areYouSure and areYouSure.Enabled then
+                    local yesButton = areYouSure.Menu.Frame.Buttons.Yes
                     if yesButton and yesButton:IsA("GuiButton") and yesButton.Visible then
-                        local parentFrame = yesButton.Parent
-                        if parentFrame then
-                            GuiService.SelectedObject = yesButton
-                            task.wait(0.05)
-                            
-                            VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-                            task.wait(0.02)
-                            VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
-                            
-                            local absPos = yesButton.AbsolutePosition
-                            local absSize = yesButton.AbsoluteSize
-                            local clickX = absPos.X + (absSize.X / 2)
-                            local clickY = absPos.Y + (absSize.Y / 2)
-                            
-                            VirtualInputManager:SendMouseButtonEvent(clickX, clickY, 0, true, game, 1)
-                            task.wait(0.02)
-                            VirtualInputManager:SendMouseButtonEvent(clickX, clickY, 0, false, game, 1)
-                            
-                            task.wait(1)
-                        end
+                        GuiService.SelectedObject = yesButton
+                        task.wait(0.05)
+                        
+                        VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+                        task.wait(0.02)
+                        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+                        
+                        local absPos = yesButton.AbsolutePosition
+                        local absSize = yesButton.AbsoluteSize
+                        local clickX = absPos.X + (absSize.X / 2)
+                        local clickY = absPos.Y + (absSize.Y / 2)
+                        
+                        VirtualInputManager:SendMouseButtonEvent(clickX, clickY, 0, true, game, 1)
+                        task.wait(0.02)
+                        VirtualInputManager:SendMouseButtonEvent(clickX, clickY, 0, false, game, 1)
+                        
+                        task.wait(1)
                     end
                 end
             end
         end)
-        task.wait(0.3)
+        task.wait(0.2)
     end
 end)
