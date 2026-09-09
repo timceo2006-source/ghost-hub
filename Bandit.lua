414e2f8f7fc7230ead65a6d488b491d096aba94d
timceo2006-source <timceo2006@gmail.com>
2026-09-09 20:25:30 +0700

    Update Bandit.lua

--- Bandit.lua
+++ Bandit.lua
@@ -148,21 +148,27 @@ end
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local Window = WindUI:CreateWindow({
-	Title = "Ghost Hub",
-	Icon = "ghost",
-	Author = "by .TiM",
-	Folder = "MyGhostHub",
-	Size = UDim2.fromOffset(580, 460),
-	MinSize = Vector2.new(560, 350),
-	MaxSize = Vector2.new(850, 560),
-	ToggleKey = Enum.KeyCode.RightControl,
-	Transparent = true,
-	Theme = "Dark",
-	Resizable = true,
-	SideBarWidth = 200,
-	BackgroundImageTransparency = 0.42,
-	HideSearchBar = true,
-	ScrollBarEnabled = false,
+    Title = "Ghost Hub",
+    Icon = "ghost",
+    Author = "by .TiM",
+    Folder = "MyGhostHub",
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(560, 350),
+    MaxSize = Vector2.new(850, 560),
+    ToggleKey = Enum.KeyCode.LeftShift,
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    User = {
+        Enabled = true,
+        Anonymous = true,
+        Callback = function()
+        end,
+    },
 })
 
 -- สร้างแท็บ Main (สำหรับ ESP และ Aimbot)
